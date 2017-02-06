5ff3220fa42ad3c5fae9cca83ca8d3332095f466
yongwoo park <karayu80@gmail.com>
2017-02-07 00:51:51 +0900

    파일 추가

---
+++ Dev/python/sphinx/src/module1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+==============================
+:mod:'module1' 모듈
+==============================
+.. moduleauthor:: 박용우 <test@gmail.com>
+
+설명
+=====
+
+모든 모듈1에 속한 Base 클래스 및 상수가 정의되어 있다.
+
+참고
+====
+
+관련링크:
+* https://docs.python.org/2/library/abc.html
+* http://stackoverflow.com/questions/4714136/python-how-to-implement-virtual-methods
+
+관련 작업자
+=================
+
+* 박용우 <yw Park>
+
+작업일지
+-----------
+
+* 2017.02.04 ywPark : 최초 문서 생성
+
+"""
+
+
+class XScanEngine (object):
+    '''
+    XSCanEngine 클래스
+
+    모든 검사 엔진들은 XScanEngine 클래스를 반드시 상속 받아야 한다.
+
+    예제 :
+    >>> class HWP3(XSanEngine):
+    ...
+
+    '''
+
+    pass
+
